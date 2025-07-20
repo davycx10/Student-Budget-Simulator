1c5e14e78bf16ca921d19e38cf8cac97d32bc615
davycx <dave.israel785@gmail.com>
2025-07-20 22:57:09 +0200

    add module for get current mont label

---
+++ utils.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+import os
+
+def format_monnaie(euros):
+    """
+    Format a number as a currency string with two decimal places.
+    """
+    return f"{euros,: .2f}€".replace(',', '')
+
+def get_month_label():
+    """
+    Get the current month label in the format.
+    """
+    return datetime.now().strftime("%Y-%m-%d")
+
+def generate_filename(prefix, ext='png'):
+    """
+    Generate a filename.
+    """
+
+    return f"{prefix}.{get_month_label()}.{ext}"
+
+def ensure_directory_exists(path):
