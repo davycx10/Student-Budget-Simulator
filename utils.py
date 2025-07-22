e719ec1c4d8e2d0552c487ec534e22a9dc2676aa
davycx <dave.israel785@gmail.com>
2025-07-22 23:02:34 +0200

    add module for return recommendation message

--- utils.py
+++ utils.py
@@ -2,12 +2,14 @@ from datetime import datetime
 import os
 
 def format_monnaie(euros):
+
     """
     Format a number as a currency string with two decimal places.
     """
     return f"{euros,: .2f}€".replace(',', '')
 
 def get_month_label():
+
     """
     Get the current month label in the format.
     """
@@ -21,6 +23,7 @@ def generate_filename(prefix, ext='png'):
     return f"{prefix}.{get_month_label()}.{ext}"
 
 def ensure_directory_exists(path):
+
     """
     Ensure that the directory exists, creating it if necessary.
     """
@@ -28,8 +31,16 @@ def ensure_directory_exists(path):
         os.makedirs(path)
     return path
 
-def recommendation_message():
+def recommendation_message(taux_depense, taux_epargne):
+
     """
-    Return a recommendation message.
+    Return a recommendation message based on spending and saving rates.
     """
-    
+    if taux_depense > 0.8:
+        return "You are spending too much. Consider reducing your expenses."
+    elif taux_epargne < 0.1:
+        return "You're not saving enough. Try to save at least 10% of your income."
+    elif taux_depense < 0.5 and taux_epargne > 0.2:
+        return "Your budget is balanced. Keep up the good work!"
+    else:
+        return "Your spending and saving rates are moderate. Keep an eye on your budget to ensure financial stability."
